df206e6fa2b07ba73688e9ee03dc5c5bedcedcb3
Tyler Woznica <tylerwoznica@outlook.com>
2025-10-13 14:52:44 -0700

    completed lab bootstrapper

--- src/main/java/com/codingnomads/ioc/lab/initial/CodingNomadDemo.java
+++ src/main/java/com/codingnomads/ioc/lab/initial/CodingNomadDemo.java
@@ -1,4 +1,18 @@
 package com.codingnomads.ioc.lab.initial;
 
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CodingNomadDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+                CodingNomadConfiguration.class);
+        CodingNomad codingNomad = ctx.getBean(CodingNomad.class);
+        System.out.println(codingNomad.createAwesomeSoftware());
+    }
 }
