49ce791294d0ee1dc9c51ce7ec8db97e8de2ee21
Tyler Woznica <tylerwoznica@outlook.com>
2025-10-17 21:48:14 -0700

    added Lombok annotations

--- src/main/java/com/codingnomads/corespring/examples/application_context/xml/Computer.java
+++ src/main/java/com/codingnomads/corespring/examples/application_context/xml/Computer.java
@@ -2,7 +2,11 @@ package com.codingnomads.corespring.examples.application_context.xml;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
-
+@AllArgsConstructor
+@Getter
+@Setter
 public class Computer {
+
 }
