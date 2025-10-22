6432959d0a08f9a0f54c8d44bd75c065884d0897
Tyler Woznica <tylerwoznica@outlook.com>
2025-10-21 17:44:19 -0700

    completed Computer class

--- src/main/java/com/codingnomads/corespring/examples/springbeans/Computer.java
+++ src/main/java/com/codingnomads/corespring/examples/springbeans/Computer.java
@@ -1,4 +1,16 @@
 package com.codingnomads.corespring.examples.springbeans;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Computer {
+    private String brand;
+    private String model;
+
+    public Computer(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
 }
